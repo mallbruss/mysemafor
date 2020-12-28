3b7c1c252902c4b60b95582d7d26923131fe41e3
mallbruss <58621572+mallbruss@users.noreply.github.com>
2020-12-29 00:00:13 +0300

    Add files via upload

---
+++ mysemafor/src/semafor/StateSemaphor.java
@@ -0,0 +1,154 @@
+package semafor;
+
+import graphicsModel.GraphicsModel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static semafor.ColorEnum.GreenTYellowRed;
+import static semafor.ColorEnum.GreenYellowTRed;
+import static semafor.ColorEnum.TGreenYellowRed;
+import static semafor.ColorEnum.NullLamp;
+
+public class StateSemaphor implements Runnable {
+
+    ChangeColor green;
+    ChangeColor red;
+    ChangeColor yellow;
+    ChangeColor state;
+    ChangeColor oldState;
+    ChangeColor nulllamp;
+    GraphicsModel gm;
+    boolean suspendFlag = false;
+    
+
+    public StateSemaphor(GraphicsModel model) {
+        green = new Green();
+        red = new Red();
+        yellow = new Yellow();
+        state = green;
+        oldState = green;
+        nulllamp = new NullLampColor();
+        gm = model;
+        suspendFlag = false;
+    }
+
+    public void changeState() {
+        gm.setColor(state.getColorEnum());
+        try {
+                Thread.sleep(state.count);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Green.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        state.changeColor();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 200; i++) {
+            changeState();
+            stop();
+        }
+
+    }
+
+    private void stop() {
+        try {
+            Thread.sleep(200);
+            synchronized (this) {
+                while (suspendFlag) {
+                    wait();
+                }
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(StateSemaphor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public synchronized void mysuspend() {
+        suspendFlag = true;
+    }
+
+    public synchronized void myresume() {
+        suspendFlag = false;
+        notify();
+    }
+
+    public abstract class ChangeColor {
+        public int count=500;
+        ColorEnum colorEnum;
+
+        public ColorEnum getColorEnum() {
+            return colorEnum;
+        }
+        
+        public abstract void changeColor();
+    }
+
+    public class Green extends ChangeColor {
+
+        public Green() {
+           colorEnum = TGreenYellowRed; 
+        }
+
+        @Override
+        public void changeColor() {
+            oldState = green;
+            state = nulllamp;
+        }
+    }
+
+    public class Red extends ChangeColor {
+
+        public Red() {
+            colorEnum = GreenYellowTRed; 
+        }
+
+        @Override
+        public void changeColor() {
+            oldState = red;
+            state = yellow;
+        }
+
+    }
+
+    public class Yellow extends ChangeColor {
+
+        public Yellow() {
+            colorEnum = GreenTYellowRed; 
+        }
+
+        @Override
+        public void changeColor() {
+            if (oldState == red) {
+                state = green;
+                oldState = yellow;
+            } 
+            else {
+                state = red;
+                oldState = yellow;
+            }
+           
+        }
+    }
+
+    public class NullLampColor extends ChangeColor {
+        int col;
+        public NullLampColor() {
+            count = 100;
+            col = 5;
+        }
+
+        @Override
+        public void changeColor() {
+            if (col > 0) {
+                colorEnum = (colorEnum == NullLamp ? TGreenYellowRed: NullLamp);
+                col--;
+            }
+            else {
+                col = 5;
+                state = yellow;
+                oldState = nulllamp;
+            }
+
+        }
+    }
+}
